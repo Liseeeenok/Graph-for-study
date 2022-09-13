1e8a3ad51894d03bc6f2f3f57eb05cb88d374e35
vlad.vedernikov.95@mail.ru <vlad.vedernikov.95@mail.ru>
2022-09-13 10:53:58 +0800

    Added redrawing of the graph

--- main.js
+++ main.js
@@ -1,4 +1,29 @@
 const ctx = document.getElementById('myChart').getContext('2d');
+const inputFile = document.querySelector('#inputFile');
+let listValues = [];
+let listNameValues = [];
+let arrBackgroundColor = [];
+let arrBorderColor = [];
+let myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: listValues,
+        datasets: [{
+            label: 'Lab',
+            data: listNameValues,
+            backgroundColor: arrBackgroundColor,
+            borderColor: arrBorderColor,
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
 
 function downloadGraph(input) {
     let file = input.files[0];
@@ -11,30 +36,35 @@ function downloadGraph(input) {
 };
 
 function generationGraph(data) {
+    myChart.destroy();
+    //console.log(inputFile);
+    inputFile.style.display = 'none';
     listValues = [];
     listNameValues = [];
     data = data.split('\n');
     //console.log(data);
     //data.splice(data.length-1,1);
-    console.log(data);
+    //console.log(data);
     for(let i = 0; i < data.length; i++) {
       str = data[i].split('|');
       str[0] = Number(str[0]);
       str[1] = Number(str[1]);
       listValues.push(str[0]);
       listNameValues.push(str[1]);
+      arrBackgroundColor.push('rgb(255, 99, 132)')
+      arrBorderColor.push('rgb(255, 99, 132)')
     }
-    console.log(listValues);
-    console.log(listNameValues);
-    const myChart = new Chart(ctx, {
+    //console.log(listValues);
+    //console.log(listNameValues);
+    myChart = new Chart(ctx, {
         type: 'bar',
         data: {
             labels: listValues,
             datasets: [{
-                label: '# of Votes',
+                label: 'Lab',
                 data: listNameValues,
-                backgroundColor: 'rgb(255, 99, 132)',
-                borderColor: 'rgb(255, 99, 132)',
+                backgroundColor: arrBackgroundColor,
+                borderColor: arrBorderColor,
                 borderWidth: 1
             }]
         },
